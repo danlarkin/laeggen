66752743fd7c27582a5fab65294ce524cd934dfc
Dan Larkin <dan@danlarkin.org>
2012-08-05 22:39:19 -0400

    urls! now defines routes instead of defpage doing it.
    This has the nice property of allowing control over ordering so that specific routes can get tested before wildcard ones.

--- src/laeggen/dispatch.clj
+++ src/laeggen/dispatch.clj
@@ -1,9 +1,8 @@
 (ns laeggen.dispatch)
 
-(defonce url-patterns
-  (atom (sorted-map-by (fn [r s] (compare (str r) (str s))))))
+(defonce url-patterns (atom ()))
 
-(defonce specials (atom {}))
+(defonce url-specials (atom {}))
 
 (defn url-matches? [uri [pattern page-fn]]
   (when-let [match (re-find pattern uri)]
@@ -12,16 +11,28 @@
         (apply page-fn request (rest match)))
       page-fn)))
 
-(defmacro defpage [regex-or-special & fn-tail]
-  `(swap! (if (keyword? ~regex-or-special)
-            specials
-            url-patterns)
-          assoc ~regex-or-special (fn ~@fn-tail)))
-
+;; TODO: memoize this in production somehow
 (defn find-match-for [uri]
   (if (keyword? uri)
-    (@specials uri)
+    (@url-specials uri)
     (->> @url-patterns
-         (pmap (partial url-matches? uri))
+         (map (partial url-matches? uri))
          (remove nil?)
          first)))
+
+(defn urls! [& patterns]
+  (let [{specials true
+         urls false} (group-by (comp keyword? first)
+                               (partition 2 2 patterns))]
+    (if (and (even? (count patterns))
+             (every? keyword? (map first specials))
+             (every? (partial instance? java.util.regex.Pattern)
+                     (map first urls))
+             (every? #(or (fn? %)
+                          (and (var? %)
+                               (fn? @%)))
+                     (map second (concat specials urls))))
+      (do
+        (swap! url-patterns (partial concat urls))
+        (swap! url-specials merge (into {} (map vec specials))))
+      (throw (Exception. "patterns are malformed")))))
--- src/laeggen/pages/default.clj
+++ src/laeggen/pages/default.clj
@@ -1,9 +1,9 @@
 (ns laeggen.pages.default
   (:require [clojure.java.io :as io]
             [clojure.stacktrace :refer [print-stack-trace]]
-            [laeggen.dispatch :refer [defpage]]))
+            [laeggen.dispatch :refer [urls!]]))
 
-(defpage #"^/assets/(.*)$" [request path]
+(defn assets [request path]
   (let [path (if (.endsWith path "/")
                (subs path 0 (dec (count path)))
                path)]
@@ -16,13 +16,18 @@
            (.endsWith path ".png")
            {:headers {"content-type" "image/png"}}))))
 
-(defpage :404 [request]
+(defn handler404 [request]
   {:status 404
    :headers {"content-type" "text/plain"}
    :body "404"})
 
-(defpage :500 [request exception]
+(defn handler500 [request exception]
   {:status 500
    :headers {"content-type" "text/plain"}
    :body (with-out-str
            (print-stack-trace exception))})
+
+(urls!
+ #"^/assets/(.*)$" assets
+ :404 handler404
+ :500 handler500)
