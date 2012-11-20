4f51f57ef69dbc8e76a250fd1f18eba67b75bbd3
Dan Larkin <dan@danlarkin.org>
2012-11-19 19:04:00 -0500

    add more data to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject laeggen "0.4-SNAPSHOT"
   :description "I guess it's an HTTP server?"
+  :url "https://github.com/danlarkin/laeggen/"
+  :license {:name "3-Clause BSD"
+            :url "http://opensource.org/licenses/BSD-3-Clause"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [aleph "0.3.0-beta8"]])
